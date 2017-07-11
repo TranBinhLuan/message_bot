2378610c0da0afb90d1a659906ee732288c3c161
TranbinhLuanj <luantranptiz@gmail.com>
2017-07-11 15:20:51 +0700

    demo

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, '127.0.0.1');
+}).listen(8080, 'calm-dawn-19611.herokuapp.com');
 console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
